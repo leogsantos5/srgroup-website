c390b692bc9743de87bf379df300920fdb2e97ea
Leonardo Santos <leogsantos5@gmail.com>
2024-06-13 19:10:55 +0100

    Final commit with bug fixes for already tested perfect deployment.

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/srgroup-website/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: "/src/index.js",
-    },
-  },
 });
